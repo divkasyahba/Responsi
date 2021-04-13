8b804f9990b89070e24de93d56a200e2f713e08a
divkasyahba <divkasyahba@students.amikom.ac.id>
2021-04-13 14:59:59 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ResponsiPemograman3394
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan(190302123, "paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "jono", 2000000);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("|No.\t NIK \t  NIM \t Nama \t \t Gaji|");
+            Console.WriteLine("\n");
+            Console.WriteLine(karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+
+            Console.WriteLine("\n Selamat Kamu mendapatkan kenaikan gaji sebesar 10% \n");
+
+            Console.WriteLine("|No.\t NIK \t  NIM \t Nama \t \t Gaji|");
+            Console.WriteLine("\n");
+            Console.WriteLine(karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemograman3394
+{
+    class karyawan
+    {
+        public int NIK { get; get; }
+        public string Nama { get; get; }
+        public double GajiBulanan { get; get; }
+        public double NaiikGajiBulanan { get; get; }
+
+        public karyawan (int NIK, string Nama, double GajiBulanan)
+        {
+            this.NIK = NIK;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else 
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+
+            double tambah= 0;
+            tambah = 0.1 * GajiBulanan;
+            NaikGajiBulanan = Convert.ToInt32(tambah) * GajiBulanan;
+
+        }
+
+      
+
+
+    }
+}
